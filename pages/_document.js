b415ddd5d5c89fc203e9a411480df8af31690287
VISHAL DUBEY <DUBEYVISHAL917@GMAIL.COM>
2024-10-02 10:28:09 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+    <meta name="google-site-verification" content="78IKF1o38uymg2UfKIzqDaJX9aoc9cxWEhnRjhJHsKo" />
       <Head />
       <body className="antialiased">
         <Main />

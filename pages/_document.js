cbc28b087f0f8decb682d411fd2f3d6d9148c3b3
VISHAL DUBEY <DUBEYVISHAL917@GMAIL.COM>
2025-08-07 04:53:55 +0530

    Add files via upload

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3688316177563716"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />

450c421654b67ad3653300783c9e1681e55f92e5
wishaldubey <dubeyvishal917@gmail.com>
2025-08-07 06:10:47 +0530

    maybe fix

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="google-site-verification" content="tVssnuo0j8EsKq4NUm7trKH32PLgjEpL_Ogs5TjsDWE" />
-        <script 
+<meta name="google-site-verification" content="78IKF1o38uymg2UfKIzqDaJX9aoc9cxWEhnRjhJHsKo" />        <script 
           async 
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3688316177563716"
           crossOrigin="anonymous"
